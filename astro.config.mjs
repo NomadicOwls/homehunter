d2c29b18282f125f4b06620b8f4f6d458260f4bf
NT <nt90@live.nl>
2023-11-11 15:18:14 +0100

    f5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
 import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
+
+// https://astro.build/config
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://homehunter.com",
-  integrations: [image({
+  site: "https://unforgettabletripstobali.com",
+  trailingSlash: 'never',
+  integrations: [tailwind(), image({
     serviceEntryPoint: "@astrojs/image/sharp"
-  }), mdx(), sitemap()]
+  }), mdx(), sitemap(), svelte()]
 });
